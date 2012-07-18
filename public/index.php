52d16c53d61d38b8c12d9551a8df5912581da8a6
Adam Lundrigan <adam@lundrigan.ca>
2012-07-18 19:05:30 -0230

    Clean up autoloader files

--- public/index.php
+++ public/index.php
@@ -5,8 +5,14 @@
  */
 chdir(dirname(__DIR__));
 
-// Setup autoloading
-include 'init_autoloader.php';
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    $loader = include 'vendor/autoload.php';
+}
+
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+}
 
 // Run the application!
 Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
